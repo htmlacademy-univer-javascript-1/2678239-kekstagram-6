61b65a70cb42654b098512edb50e91d074dcfb37
Vimato <monst3k003@gmail.com>
2025-12-04 21:12:19 +0700

    Delete duplicated methods

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-import {createPostGenerator, createCommentGenerator, createPictureGenerator} from './data.js';
+import {createPostGenerator, createCommentGenerator} from './data.js';
 import {renderPictures} from './pictures.js';
 
 function main() {
   const commentGenerator = createCommentGenerator();
   const postGenerator = createPostGenerator(commentGenerator);
-  const picturesGenerator = createPictureGenerator();
   const size = 25 ;
-  const pictures = [];
   const posts = [];
   for (let i = 1; i <= size; i++) {
     posts.push(postGenerator());
-    pictures.push(picturesGenerator());
   }
-  renderPictures(pictures);
+  renderPictures(posts);
 }
 
 main();
