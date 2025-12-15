2366507f6aa2507500eee7630213a6a0eb57d47a
Vimato <monst3k003@gmail.com>
2025-12-15 19:42:59 +0700

    Add form validation

---
+++ js/form.js
@@ -0,0 +1,74 @@
+import {initValidator} from './validator.js';
+
+const pristine = initValidator();
+
+function createCloseHandler(imgUpload) {
+  const overlay = document.querySelector('.img-upload__overlay');
+  const submitButton = document.querySelector('.img-upload__submit');
+  const closeButton = document.querySelector('.img-upload__cancel');
+  const hashtagElement = document.querySelector('.text__hashtags');
+  const descriptionElements = document.querySelector('.text__description');
+
+  function onClick() {
+    close();
+  }
+
+  function onKeyDown(evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      close();
+    }
+  }
+
+  function close() {
+    overlay.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    imgUpload.value = '';
+    hashtagElement.value = '';
+    descriptionElements.value = '';
+    hashtagElement.removeEventListener('keydown', onEsc);
+    descriptionElements.removeEventListener('keydown', onEsc);
+    submitButton.removeEventListener('click', onSubmit);
+    closeButton.removeEventListener('click', onClick);
+    document.removeEventListener('keydown', onKeyDown);
+  }
+
+  closeButton.addEventListener('click', onClick);
+  document.addEventListener('keydown', onKeyDown);
+}
+
+function onSubmit(evt) {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+}
+
+function onEsc(evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+}
+
+function openUploader() {
+  const submitButton = document.querySelector('.img-upload__submit');
+  const overlay = document.querySelector('.img-upload__overlay');
+  const imgUpload = document.querySelector('.img-upload__input');
+  const hashtagElement = document.querySelector('.text__hashtags');
+  const descriptionElements = document.querySelector('.text__description');
+
+  overlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  hashtagElement.addEventListener('keydown', onEsc);
+  descriptionElements.addEventListener('keydown', onEsc);
+  submitButton.addEventListener('click', onSubmit);
+  createCloseHandler(imgUpload);
+}
+
+
+function initImageUploader() {
+  const imgUpload = document.querySelector('.img-upload__input');
+  imgUpload.addEventListener('change', openUploader);
+}
+
+
+export {initImageUploader};
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import {generatePosts} from './data.js';
 import {renderPictures} from './pictures.js';
 import {createPictureClickHandler} from './big_picture.js';
+import {initImageUploader} from './form.js';
 
 function main() {
   const posts = generatePosts(25);
   renderPictures(posts);
   createPictureClickHandler(posts);
+  initImageUploader();
 }
 
 main();
